7d4bbb4bad2eb3b3ce3200be63956c49d0156ad6
SWCIITGWeb <swciitghy@gmail.com>
2025-04-26 19:58:39 +0000

    Refactor admin panel and frontend configurations for consistency and clarity

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,6 @@ import AllClubsPage from "./pages/AllClubsPage";
 import EachClubPage from "./pages/ClubPage";
 import EachEventPage from "./pages/EventPage";
 const BASEURL = process.env.REACT_APP_BASEURL||"/cultural-board";
-
 function App() {
   return (
     <BrowserRouter basename={`${BASEURL}`} >
